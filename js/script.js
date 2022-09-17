a9347d055076d7a49e83ecce02b5b00ddcb3d82f
Daniil <daniilzavodsoj@gmail.com>
2022-09-17 22:34:52 +0300

    new

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ if (reviewsSwiper) {
     },
   });
 }
+document.querySelectorAll("a.navigation__link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = this.getAttribute("href").substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = 0;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+});
