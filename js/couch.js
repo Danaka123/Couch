46be76c02ab8633a493c4cd12434520fe30b0aab
Daniil <daniilzavodsoj@gmail.com>
2022-11-25 12:56:03 +0100

    new

--- js/couch.js
+++ js/couch.js
@@ -1,30 +1,11 @@
-"use strict";
-
-const reviewsSwiper = document.querySelector(".popularcouch-swiper");
-
-if (reviewsSwiper) {
-  const swiper = new Swiper(".popularcouch-swiper", {
-    // Optional parameters
-    autoHeught: true,
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-}
-document.querySelectorAll("a.navigation__link").forEach((link) => {
+document.querySelectorAll("a.about-link").forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = this.getAttribute("href").substring(1);
-
     const scrollTarget = document.getElementById(href);
-
     const topOffset = 0;
     const elementPosition = scrollTarget.getBoundingClientRect().top;
     const offsetPosition = elementPosition - topOffset;
-
     window.scrollBy({
       top: offsetPosition,
       behavior: "smooth",
@@ -32,7 +13,7 @@ document.querySelectorAll("a.navigation__link").forEach((link) => {
   });
 });
 
-const btn = document.querySelector(".timetable-item");
+const btn = document.querySelector(".button1");
 const modalOverlay = document.querySelector(".modal-overlay");
 const span = document.querySelector(".close");
 
--- js/input.js
+++ js/input.js
@@ -16,3 +16,21 @@ input.addEventListener("click", onInput);
 function isEmailValid(value) {
   return EMAIL_REGEXP.test(value);
 }
+
+const btnReg = document.querySelector(".button-input__registration");
+const btnInput = document.querySelector(".button-input__entrance");
+
+btnReg.addEventListener("click", () => {
+  document.querySelector(".input").style.display = "none";
+  document.querySelector(".register").style.display = "flex";
+});
+btnInput.addEventListener("click", () => {
+  document.querySelector(".register").style.display = "none";
+  document.querySelector(".input").style.display = "flex";
+});
+
+// window.onclick = (e) => {
+//   if (e.target == modalOverlay) {
+//     modalOverlay.style.display = "none";
+//   }
+// };
